513c38ec01b6c43240a41568997c073b04632b6b
Optimusys <159470517+epltv1@users.noreply.github.com>
2026-03-09 03:44:36 +0300

    Delete app directory

--- app/layout.js
+++
@@ -1,8 +0,0 @@
-import './globals.css';
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="bg-white dark:bg-[#0f1012]">{children}</body>
-    </html>
-  );
-}
