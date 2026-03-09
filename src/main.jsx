922a4058d49e05d1f7361b4f2d50ad3ece06f3ce
Optimusys <159470517+epltv1@users.noreply.github.com>
2026-03-09 18:02:43 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createClient } from '@supabase/supabase-js'
 import App from './App'
+import './index.css'
 
-// Initialize here and export it
 export const supabase = createClient(
   'https://jvpnulbwrjjryrqbzfpa.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2cG51bGJ3cmpqcnlycWJ6ZnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MzU4MTEsImV4cCI6MjA4ODQxMTgxMX0.BDrz2DTfybunOlU0nuSNvURu8-ePgEK_pfrXIrxa7Ss' 
