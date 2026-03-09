0334f14c63f4ced91f27fd3bd599c820712cc01f
Optimusys <159470517+epltv1@users.noreply.github.com>
2026-03-09 03:54:23 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Chat from './components/Chat'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-4 bg-[#0f1012]">
+      <Chat />
+    </div>
+  )
+}
+
+export default App
